490e1fe1821bb0534dadb5b3d850ed76cd6612ad
Louis Roche <louis.roche@icloud.com>
2025-05-12 10:44:13 +0100

    size fix

--- utils.py
+++ utils.py
@@ -152,9 +152,9 @@ class ReplayBuffer(object):
 		idx = torch.arange(self.ptr, (self.ptr+len(a_array)) %self.max_size)
 		self.s[idx] = torch.from_numpy(np.stack(s_array, axis=0)).to(self.dvc)
 		self.a[idx] = torch.tensor(a_array).unsqueeze(-1).to(self.dvc)
-		self.r[idx] = torch.tensor(r_array).unsqueeze(-1).float().to(self.dvc)
+		self.r[idx] = torch.tensor(r_array).float().to(self.dvc)
 		self.s_next[idx] = torch.from_numpy(np.stack(s_next_array, axis=0)).to(self.dvc)
-		self.dw[idx] = torch.tensor(dw_array).unsqueeze(-1).to(self.dvc)
+		self.dw[idx] = torch.tensor(dw_array).to(self.dvc)
 		self.ptr = (self.ptr + len(a_array)) % self.max_size
 		self.size = min(self.size + len(idx), self.max_size)
 		
